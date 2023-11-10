7ef2478b5e56d3cbb8f5405c6880a8de5128f848
Khasan-ali <toxtayevhasanali@gmail.com>
2023-11-10 04:59:25 -0500

    elheader

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-const elBtn = document.querySelector('.header-btn');
-const elNavbar = document.querySelector('.header-nav');
-const elClose = document.querySelector('.header-close');
+// const elBtn = document.querySelector('.header-btn');
+// const elNavbar = document.querySelector('.header-nav');
+// const elClose = document.querySelector('.header-close');
 
-elBtn.addEventListener('click', ()=> {
-    elNavbar.style.display = "flex";
-    elBtn.style.display = 'none';
-    elClose.style.display = 'inline';
-})
+// elBtn.addEventListener('click', ()=> {
+//     elNavbar.style.display = "flex";
+//     elBtn.style.display = 'none';
+//     elClose.style.display = 'inline';
+// })
 
-elClose.addEventListener('click', ()=> {
-    elNavbar.style.display = "none";
-    elBtn.style.display = 'inline';
-    elClose.style.display = 'none';
-})
+// elClose.addEventListener('click', ()=> {
+//     elNavbar.style.display = "none";
+//     elBtn.style.display = 'inline';
+//     elClose.style.display = 'none';
+// })
