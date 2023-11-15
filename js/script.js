42482ffbe6bff759e73231e04c341d557c0ede23
Khasan-ali <toxtayevhasanali@gmail.com>
2023-11-15 08:38:43 -0500

    radios ful

--- js/script.js
+++ js/script.js
@@ -1,13 +1,50 @@
 const elBtn = document.querySelector('.header-btn');
 const elHeader = document.querySelector('.header')
-const elNavbar = document.querySelector('.header-nav');
 const elClose = document.querySelector('.header-close');
+
+const elRadiosHeadingLink = document.querySelectorAll('.radios__left-heading-link')
+const elRadiosHeading = document.querySelectorAll('.radios__left-heading')
+
+elRadiosHeadingLink[0].addEventListener('click', ()=> {
+    elRadiosHeading[0].classList.toggle('radios__left-heading-open')
+})
+elRadiosHeadingLink[1].addEventListener('click', ()=> {
+    elRadiosHeading[1].classList.toggle('radios__left-heading-open')
+})
+elRadiosHeadingLink[2].addEventListener('click', ()=> {
+    elRadiosHeading[2].classList.toggle('radios__left-heading-open')
+})
+elRadiosHeadingLink[3].addEventListener('click', ()=> {
+    elRadiosHeading[3].classList.toggle('radios__left-heading-open')
+})
+elRadiosHeadingLink[4].addEventListener('click', ()=> {
+    elRadiosHeading[4].classList.toggle('radios__left-heading-open')
+})
+
+const elRadiosBtn = document.querySelectorAll('.radios__right-select')
+const elRadiosSection = document.querySelectorAll('.radios__section')
+
+elRadiosBtn[0].addEventListener('click', ()=> {
+    elRadiosSection[0].classList.toggle('radios__right-select-open')
+})
+elRadiosBtn[1].addEventListener('click', ()=> {
+    elRadiosSection[1].classList.toggle('radios__right-select-open')
+})
+elRadiosBtn[2].addEventListener('click', ()=> {
+    elRadiosSection[2].classList.toggle('radios__right-select-open')
+})
+elRadiosBtn[3].addEventListener('click', ()=> {
+    elRadiosSection[3].classList.toggle('radios__right-select-open')
+})
+elRadiosBtn[4].addEventListener('click', ()=> {
+    elRadiosSection[4].classList.toggle('radios__right-select-open')
+})
+
+
 
 
 elBtn.addEventListener('click', ()=> {
     elHeader.classList.toggle('header--open')
-    // elBtn.style.display = 'none';
-    // elClose.style.display = 'inline-block';
 })
 
 elClose.addEventListener('click', ()=> {
